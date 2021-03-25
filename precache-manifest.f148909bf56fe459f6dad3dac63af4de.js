6af73c37c27a42bf8f38f441757cb4bf3c340b59
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2021-03-25 19:22:36 +0000

    Updates

---
+++ precache-manifest.f148909bf56fe459f6dad3dac63af4de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5435977c35ae9d13d23f3bbbe4404ece",
+    "url": "/sarauras1/reven/index.html"
+  },
+  {
+    "revision": "90524ac0a1c4b891fa57",
+    "url": "/sarauras1/reven/static/css/main.f957a60e.chunk.css"
+  },
+  {
+    "revision": "68ea58685e29b69c5a6f",
+    "url": "/sarauras1/reven/static/js/2.ae4d3e85.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sarauras1/reven/static/js/2.ae4d3e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90524ac0a1c4b891fa57",
+    "url": "/sarauras1/reven/static/js/main.a156590e.chunk.js"
+  },
+  {
+    "revision": "a98dff6c28fe091d009a",
+    "url": "/sarauras1/reven/static/js/runtime-main.9286518b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/sarauras1/reven/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/sarauras1/reven/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
